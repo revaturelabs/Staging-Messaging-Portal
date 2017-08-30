ca5435162328dea2defac39b1920aa3f45dfcac0
dataricc <rcsmith.dev@gmail.com>
2017-08-30 15:57:23 -0400

    RicS - Major update to skeleton project: AOP, Log4j2, User bean-service-dao, and some fixes for deployment still in progress.

--- src/main/java/com/revature/smp/SmpApplication.java
+++ src/main/java/com/revature/smp/SmpApplication.java
@@ -2,15 +2,16 @@ package com.revature.smp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+@ComponentScan("com.revature.smp.*")
 @EnableScheduling
 @EnableJpaRepositories("com.revature.smp.dao")
 public class SmpApplication {
@@ -32,20 +33,5 @@ public class SmpApplication {
 	{
 		return builder.build();
 	}
-	
-	// @Bean
-	// public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	// return args -> {
-	//
-	// System.out.println(
-	// "Let's inspect the beans provided by Spring Boot:");
-	//
-	// String[] beanNames = ctx.getBeanDefinitionNames();
-	// Arrays.sort(beanNames);
-	// for (String beanName : beanNames) {
-	// System.out.println(beanName);
-	// }
-	//
-	// };
-	// }
+
 }
