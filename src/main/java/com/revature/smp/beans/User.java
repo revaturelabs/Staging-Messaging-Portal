ebad52ed74eb4c8f66057a4c6ff9265ec381d779
dataricc <rcsmith.dev@gmail.com>
2017-08-31 15:04:00 -0400

    Finished User bean and it's dependent beans.

--- src/main/java/com/revature/smp/beans/User.java
+++ src/main/java/com/revature/smp/beans/User.java
@@ -1,12 +1,16 @@
 package com.revature.smp.beans;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -19,6 +23,7 @@ import org.springframework.stereotype.Component;
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 6104022944061620088L;
+	
 	@Id
 	@GeneratedValue(generator = "SMP_USER_SEQ", strategy = GenerationType.SEQUENCE)
 	@GenericGenerator(name="SMP_USER_SEQ", strategy="org.hibernate.id.enhanced.SequenceStyleGenerator", parameters={
@@ -27,103 +32,155 @@ public class User implements Serializable{
 			@Parameter(name="initial_value",value="1"),
 			@Parameter(name="increment_size",value="1")
 	})
+	
 	@Column(name="USER_ID")
 	private int userId;
 	
-//	@Column(name="EMAIL")
-//	private String email;
-//	
-//	@Column(name="FIRSTNAME")
-//	private String firstName;
-//	
-//	@Column(name="LASTNAME")
-//	private String lastName;
-//	
-//	@Column(name="SALESFORCE")
-//	private Integer salesforce;
-//	
-//	@Column(name="RECRUITER_ID")
-//	private Integer recruiterId;
-//	
-//	@ManyToOne(fetch=FetchType.EAGER,targetEntity = Role.class)
-//	@JoinColumn(name="ROLE_ID")
-//	private Role role;
-//	
-//	@Column(name="DATE_PASS_ISSUED")
-//	private String datePassIssued;
-//	
-//	@Transient
-//    private String format;
-//
-//	@Transient
-//	private Integer grade;
-//	
-//	public User() {
-//		super();
-//	}
-//
-//	public String getFormat() { return format; }
-//	public void setFormat(String format) { this.format = format; }
-//	public Integer getGrade() { return grade; }
-//	public void setGrade(Integer grade) { this.grade = grade; }
-//	public int getUserId() {
-//		return userId;
-//	}
+	@Column(name="USERNAME")
+	private int username;
+	
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	
+	@Column(name="LAST_NAME")
+	private String lastName;
+	
+	@Column(name="EMAIL")
+	private String email;
+	
+	@ManyToOne(fetch=FetchType.EAGER, targetEntity = Location.class)
+	@JoinColumn(name="LOCATION_ID")
+	private Location location;
+	
+	@ManyToOne(fetch=FetchType.EAGER, targetEntity = Status.class)
+	@JoinColumn(name="STATUS_ID")
+	private Status status;
+	
+	@ManyToOne(fetch=FetchType.EAGER, targetEntity = Role.class)
+	@JoinColumn(name="ROLE_ID")
+	private Role role;
+	
+	@Column(name="LOGGED")
+	private String logged;
+	
+	@Column(name="USE_TEMP")
+	private String useTemp;
+	
+	@Column(name="ACTIVE")
+	private String active;
+	
+	@Column(name="CREATED")
+	private Date created;
+	
+	public User() {
+		super();
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-//	public String getEmail() {
-//		return email;
-//	}
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//	public String getFirstName() {
-//		return firstName;
-//	}
-//	public void setFirstName(String firstName) {
-//		this.firstName = firstName;
-//	}
-//	public String getLastName() {
-//		return lastName;
-//	}
-//	public void setLastName(String lastName) {
-//		this.lastName = lastName;
-//	}
-//	public Integer getSalesforce() {
-//		return salesforce;
-//	}
-//	public void setSalesforce(Integer salesforce) {
-//		this.salesforce = salesforce;
-//	}
-//	public Integer getRecruiterId() {
-//		return recruiterId;
-//	}
-//	public void setRecruiterId(Integer recruiterId) {
-//		this.recruiterId = recruiterId;
-//	}
-//
-//	public Role getRole() {
-//		return role;
-//	}
-//
-//	public void setRole(Role role) {
-//		this.role = role;
-//	}
-//
-//	public String getDatePassIssued() {
-//		return datePassIssued;
-//	}
-//	public void setDatePassIssued(String datePassIssued) {
-//		this.datePassIssued = datePassIssued;
-//	}
-//	public static long getSerialversionuid() {
-//		return serialVersionUID;
-//	}
-//	@Override
-//	public String toString() {
-//		return "User [userId=" + userId + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
-//				+ ", salesforce=" + salesforce + ", recruiterId=" + recruiterId + ", role=" + role + ", datePassIssued="
-//				+ datePassIssued + "]";
-//	}
+
+	public int getUsername() {
+		return username;
+	}
+
+	public void setUsername(int username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public String getLogged() {
+		return logged;
+	}
+
+	public void setLogged(String logged) {
+		this.logged = logged;
+	}
+
+	public String getUseTemp() {
+		return useTemp;
+	}
+
+	public void setUseTemp(String useTemp) {
+		this.useTemp = useTemp;
+	}
+
+	public String getActive() {
+		return active;
+	}
+
+	public void setActive(String active) {
+		this.active = active;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", email=" + email + ", location=" + location + ", status=" + status + ", role=" + role
+				+ ", logged=" + logged + ", useTemp=" + useTemp + ", active=" + active + ", created=" + created + "]";
+	}
+
 }
