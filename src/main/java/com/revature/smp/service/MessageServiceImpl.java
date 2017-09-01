726ee32fa8f656e6a7f11f9eb91175affa471f4c
Samuel Levy <samuelnlevy@gmail.com>
2017-09-01 17:39:14 -0400

    altered message dao and service to use paging

--- src/main/java/com/revature/smp/service/MessageServiceImpl.java
+++ src/main/java/com/revature/smp/service/MessageServiceImpl.java
@@ -1,8 +1,5 @@
 package com.revature.smp.service;
 
-import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.revature.smp.beans.Message;
@@ -20,22 +17,12 @@ public class MessageServiceImpl implements MessageService {
 
 	@Override
 	public Message getMostRecentBlob(int messageRoomId) {
-		List<Message> lm = mdao.findByMessageRoomIdOrderByMessageBlobIdDesc(messageRoomId);
-		if(lm.size()>0) {
-			return lm.get(0);
-		}else {
-			return null;
-		}
+		return mdao.getMostRecent(messageRoomId);
 	}
 
 	@Override
 	public Message getPreviousBlob(int messageRoomId,int messageBlobId) {
-		List<Message> lm = mdao.findByMessageRoomIdAndMessageBlobIdLessThanOrderByMessageBlobIdDesc(messageRoomId, messageBlobId);
-		if(lm.size()>0) {
-			return lm.get(0);
-		}else {
-			return null;
-		}
+		return mdao.getPrevious(messageRoomId, messageBlobId);
 	}
 
 }
