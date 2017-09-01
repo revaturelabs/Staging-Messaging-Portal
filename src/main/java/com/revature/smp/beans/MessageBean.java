aa2e61a941378af852b61892431ab372e696566c
dataricc <rcsmith.dev@gmail.com>
2017-09-01 12:05:15 -0400

    Message Controller in progress

---
+++ src/main/java/com/revature/smp/beans/MessageBean.java
@@ -0,0 +1,47 @@
+package com.revature.smp.beans;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageBean implements Serializable {
+
+	private static final long serialVersionUID = -6407809690643413873L;
+
+	private int msgId;
+	
+	private String message;
+	
+	public MessageBean() {
+		super();
+	}
+	
+	public MessageBean(int msgId, String message) {
+		super();
+		this.msgId = msgId;
+		this.message = message;
+	}
+
+	public int getMsgId() {
+		return msgId;
+	}
+
+	public void setMsgId(int msgId) {
+		this.msgId = msgId;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "MessageBean [msgId=" + msgId + ", message=" + message + "]";
+	}
+	
+}
