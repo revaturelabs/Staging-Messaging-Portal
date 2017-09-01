c0cf9f6506c6d138305658dba3cfacff5dd01daa
Douglas Shoemaker <dougshmkr@gmail.com>
2017-09-01 10:59:18 -0400

    Fixing error with Rest and Web pages not loading
    Adding basic test object for testing rest endpoint

--- src/main/java/com/revature/smp/HelloController.java
+++ src/main/java/com/revature/smp/HelloController.java
@@ -1,7 +1,14 @@
 package com.revature.smp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.smp.beans.Country;
 
 @RestController
 public class HelloController {
@@ -11,4 +18,37 @@ public class HelloController {
 		return "Greetings from Spring Boot!";
 	}
 	
+	@RequestMapping(value = "/countries", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<Country> getCountries() {
+		List<Country> listOfCountries = new ArrayList<Country>();
+		listOfCountries = createCountryList();
+		return listOfCountries;
+	}
+	
+	@RequestMapping(value = "/country/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+	public Country getCountryById(@PathVariable int id) {
+		List<Country> listOfCountries = new ArrayList<Country>();
+		listOfCountries = createCountryList();
+		for (Country country : listOfCountries) {
+			if (country.getId() == id)
+				return country;
+		}
+		
+		return null;
+	}
+	
+	// Utiliy method to create country list.
+	public List<Country> createCountryList() {
+		Country indiaCountry = new Country(1, "India");
+		Country chinaCountry = new Country(4, "China");
+		Country nepalCountry = new Country(3, "Nepal");
+		Country bhutanCountry = new Country(2, "Bhutan");
+		
+		List<Country> listOfCountries = new ArrayList<Country>();
+		listOfCountries.add(indiaCountry);
+		listOfCountries.add(chinaCountry);
+		listOfCountries.add(nepalCountry);
+		listOfCountries.add(bhutanCountry);
+		return listOfCountries;
+	}
 }
---
+++ src/main/java/com/revature/smp/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.revature.smp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("greeting.html");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("classpath:/static/");
+	}
+	
+}
