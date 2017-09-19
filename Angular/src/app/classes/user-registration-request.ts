255f406610ec35e30432e42561f6085c03b0d50f
Carlos Rubiano <carlosdrubiano@gmail.com>
2017-09-19 10:41:46 -0400

    deletion

--- Angular/src/app/classes/user-registration-request.ts
+++
@@ -1,7 +0,0 @@
-export class UserRegistrationRequest {
-
-  constructor(public firstName: string,
-              public lastName: string,
-              public email: string,
-              public location: string) {}
-}
