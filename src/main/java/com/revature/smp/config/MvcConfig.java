854be4aadfd9de83020157fe1fe741631f9031ca
Stanley Chouloute <chouloute1@gmail.com>
2017-09-20 10:52:00 -0400

    updated

--- src/main/java/com/revature/smp/config/MvcConfig.java
+++ src/main/java/com/revature/smp/config/MvcConfig.java
@@ -1,15 +1,37 @@
 package com.revature.smp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * The Class MvcConfig.
+ */
 @Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter{
-
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry)
+	 */
 	@Override
-	public void addViewControllers(ViewControllerRegistry reg){
-		reg.addViewController("/thing").setViewName("greeting");
-		
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("greeting.html");
 	}
+	
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry)
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("classpath:/static/");
+	}
+	
 }
