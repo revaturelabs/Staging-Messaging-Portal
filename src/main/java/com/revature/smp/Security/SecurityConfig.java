9a8d36a261dd4826b48c0491e60dd6a7408c60d8
adambaker <spencerbakeradam@gmail.com>
2017-09-21 10:59:53 -0400

    controllers and security

---
+++ src/main/java/com/revature/smp/Security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.revature.smp.Security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Override
+	protected void configure (HttpSecurity http) throws Exception{
+		
+		http.
+			authorizeRequests()
+			.anyRequest()
+			.permitAll();
+		
+	}
+	
+
+}
