8d735b3c0fc011cbd860e0a3a9261a7c9e4c0d3e
@ForSerious <forxxserious@gmail.com>
2017-10-17 11:09:45 -0400

    Trying out some changes to see if they will fix spring security.

--- src/main/java/com/revature/smp/Security/SecurityConfig.java
+++ src/main/java/com/revature/smp/Security/SecurityConfig.java
@@ -1,15 +1,17 @@
 package com.revature.smp.Security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@Configuration
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {		
 	@Override
 	protected void configure (HttpSecurity http) throws Exception
 	{	
-		http.authorizeRequests().anyRequest().permitAll();	
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated();	
 	}
 }
