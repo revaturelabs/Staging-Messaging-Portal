cbdb0084081f7170149413bb5f6c686c52d1e1d8
@ForSerious <forxxserious@gmail.com>
2017-10-31 11:24:26 -0400

    Added status codes and login logic to the messaging controller.

--- src/main/java/com/revature/smp/controllers/MessageController.java
+++ src/main/java/com/revature/smp/controllers/MessageController.java
@@ -3,9 +3,7 @@ package com.revature.smp.controllers;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -31,23 +29,31 @@ import com.revature.smp.services.UserService;
 public class MessageController {
 	
 	@Autowired
-	MessageService msgSvc;
-	
+	MessageService msgSvc;	
 	@Autowired
-	UserService userSvc;
-	
+	UserService userSvc;	
 	@Autowired
 	HttpSession session;
 	/**
 	 * In order to post a new message, the user must be logged on.
-	 * User is identified by the cookie the supply, and matched with the logged in users in the session.
+	 * User is identified by the cookie supplied, and matched with the logged in users in the session.
 	 * @param message: The message that the user wishes to post.
 	 * @param WhosCookie: a cookie just holding the username.
-	 * @return
+	 * @return Http status code:
+	 * UNAUTHORIZED: user did not send the YonToken cookie or they are not logged in.
+	 * INTERNAL_SERVER_ERROR: The system doesn't know how to add messages to the database.
+	 * CREATED: Message successfully added.
+	 * NO_CONTENT: Message not added for no good reason.
 	 */
 	@RequestMapping(value="/post", method=RequestMethod.POST)
 	public void postMessage(@RequestBody Message message, @CookieValue("YonToken") String WhosCookie, HttpServletResponse response) 
 	{
+		User user = (User) session.getAttribute(WhosCookie);
+		if(user == null)
+		{
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
 		boolean success = false;
 		while (!success) 
 		{
@@ -77,56 +83,66 @@ public class MessageController {
 	 * @return A list of messages.
 	 */
 	@RequestMapping(value="/fetch-room/{roomId}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Message>> fetchMessagesByRoomId(@PathVariable Integer roomId,HttpServletResponse response)
+	public ResponseEntity<List<Message>> fetchMessagesByRoomId(@PathVariable Integer roomId, HttpServletResponse response)
 	{
 		List<Message> messageList = new ArrayList<Message>();		
 		messageList = msgSvc.getMessagesByRoomId(roomId);		
 		return new ResponseEntity<List<Message>>(messageList,HttpStatus.OK);
 	}
-	
+	/**
+	 * Pulls the public messages and puts them in a list.
+	 * @return OK: The list in json form.
+	 * INTERNAL_SERVER_ERROR: Someone doesn't know how to write good hibernate.
+	 */
 	@RequestMapping(value="/fetch-room/public", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, Object> fetchPublicMessages()
+	public ResponseEntity<List<Message>> fetchPublicMessages(HttpServletResponse response)
 	{
-		List<Message> messageList = new ArrayList<Message>();
-		
-		try {
+		List<Message> messageList = new ArrayList<Message>();		
+		try
+		{
 			messageList = msgSvc.getMessagesByRoomName("public");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-
-		Map<String, Object> responseMap = new HashMap<String, Object>();
-		responseMap.put("messages", messageList);
-		
-		return responseMap;
+		catch (SQLException e)
+		{
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<List<Message>>(messageList, HttpStatus.OK);
 	}
-
+	/**
+	 * Pulls the private messages from the private room. (Not sure how this will work for the manager)
+	 * @param WhosCookie Will hold the username.
+	 * @param response 
+	 * @return OK: The json form of the private messages.
+	 * UNAUTHORIZED: user has not supplied YonToken cookie or is not logged in.
+	 * INTERNAL_SERVER_ERROR: Someone doesn't know how to write good hibernate.
+	 * NO_CONTENT: This user has no private room.
+	 */
 	@RequestMapping(value="/fetch-room/private", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, Object> fetchPrivateMessages(@RequestBody User user)
+	public ResponseEntity<List<Message>> fetchPrivateMessages(@CookieValue("YonToken") String WhosCookie, HttpServletResponse response)
 	{
-		List<Message> messageList = new ArrayList<Message>();
-		
-		System.out.println(user);
-		
-		String privRoomName = (user != null && user.getUsername() != null
-				&& !user.getUsername().isEmpty() 
-				? ("priv_"+user.getUsername()) : null);
-		
+		User user = (User) session.getAttribute(WhosCookie);
+		if(user == null)
+		{
+			return new ResponseEntity<List<Message>>(HttpStatus.UNAUTHORIZED);
+		}
+		List<Message> messageList = new ArrayList<Message>();		
+		String privRoomName = (user.getUsername() != null && !user.getUsername().isEmpty() 
+				? ("priv_"+user.getUsername()) : null);		
 		if (privRoomName != null)
 		{
-			try {
+			try
+			{
 				messageList = msgSvc.getMessagesByRoomName(privRoomName);
-			} catch (SQLException e) {
-				System.out.println("frak this message private controller fetch thing");
-				e.printStackTrace();
+			}
+			catch (SQLException e)
+			{
+				return new ResponseEntity<List<Message>>(HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 		}
-		
-		Map<String, Object> responseMap = new HashMap<String, Object>();
-		responseMap.put("messages", messageList);
-		
-		return responseMap;
+		else
+		{
+			return new ResponseEntity<List<Message>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Message>>(messageList, HttpStatus.OK);
 	}
-	
 }
